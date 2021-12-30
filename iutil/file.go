5dd5418475b1e11083383a51f64561b346e5fa4b
weiyuhao <weiyuhao@zuoyebang.com>
2021-12-30 19:40:53 +0800

    feat(iutil): fix LoadYaml

--- iutil/file.go
+++ iutil/file.go
@@ -7,10 +7,28 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
+const DefaultRootPath = "."
+
+var rootPath string
+
+// SetRootPath 设置应用的根目录
+func SetRootPath(r string) {
+	rootPath = r
+}
+
+// RootPath 返回应用的根目录
+func GetRootPath() string {
+	if rootPath != "" {
+		return rootPath
+	} else {
+		return DefaultRootPath
+	}
+}
+
 // LoadYaml 加载yaml 文件内容到结构体
 func LoadYaml(filename, subPath string, s interface{}) {
 	var path string
-	path = filepath.Join(subPath, filename)
+	path = filepath.Join(GetRootPath(), subPath, filename)
 
 	if yamlFile, err := os.ReadFile(path); err != nil {
 		panic(filename + " get error: %v " + err.Error())
